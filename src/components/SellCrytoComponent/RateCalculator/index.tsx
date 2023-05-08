729d9c0c2d82b007d81f8339be70e089d4a32019
egileekada <egileoniso.ekada@gmail.com>
2023-05-08 16:29:57 +0100

    update on transaction

--- src/components/SellCrytoComponent/RateCalculator/index.tsx
+++ src/components/SellCrytoComponent/RateCalculator/index.tsx
@@ -31,10 +31,7 @@ export default function RateCalculator() {
 
         if(coinData && value){
             exchangeRate()
-        } 
-
-        console.log("hell");
-        
+        }  
 
     }, [coinData, value])
 
