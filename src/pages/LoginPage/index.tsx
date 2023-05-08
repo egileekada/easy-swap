77f52f66eab3e2321fdf600a7930681683f8a41e
egileekada <egileoniso.ekada@gmail.com>
2023-05-08 11:43:35 +0100

    update on google one click login

--- src/pages/LoginPage/index.tsx
+++ src/pages/LoginPage/index.tsx
@@ -8,6 +8,7 @@ import * as yup from 'yup'
 import { useFormik } from 'formik'; 
 import { useLoginCallback } from '../../action/useAuth'
 import { useToast } from '@chakra-ui/react'
+import GoogleButton from '../../components/GoogleButton'
 
 export default function LoginPage() {
 
@@ -124,7 +125,7 @@ export default function LoginPage() {
                     <a href='/resetpwd' className=' font-semibold ' >Forget password</a>
                 </div>
                 <ButtonComponent type="submit" disabled={!formik.isValid} bgcolor={formik.isValid ? " bg-[#303179] text-white ": ""} name={loading ? "Loading...": "Log in"} />
-                <div role='button' className=' flex items-center lg:bg-transparent bg-[#F2F4F7] rounded h-[45px]  justify-center font-medium text-[#344054] mt-4 lg:mt-8 gap-2 ' >
+                {/* <div role='button' className='  flex items-center lg:bg-transparent bg-[#F2F4F7] rounded h-[45px]  justify-center font-medium text-[#344054] mt-4 lg:mt-8 gap-2 ' >
                     <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M22.5005 12.2336C22.5005 11.3702 22.4291 10.7402 22.2744 10.0869H12.2148V13.9835H18.1196C18.0006 14.9519 17.3577 16.4102 15.9291 17.3902L15.9091 17.5206L19.0897 19.9354L19.3101 19.9569C21.3338 18.1252 22.5005 15.4302 22.5005 12.2336Z" fill="#4285F4"/>
                         <path d="M12.212 22.4996C15.1048 22.4996 17.5334 21.5662 19.3072 19.9562L15.9263 17.3895C15.0215 18.0078 13.8072 18.4395 12.212 18.4395C9.37874 18.4395 6.974 16.6079 6.11678 14.0762L5.99113 14.0866L2.68388 16.595L2.64062 16.7128C4.4025 20.1428 8.02155 22.4996 12.212 22.4996Z" fill="#34A853"/>
@@ -132,7 +133,8 @@ export default function LoginPage() {
                         <path d="M12.2121 5.55997C14.224 5.55997 15.5811 6.41163 16.3549 7.12335L19.3787 4.23C17.5216 2.53834 15.1049 1.5 12.2121 1.5C8.02158 1.5 4.40251 3.85665 2.64062 7.28662L6.1049 9.92332C6.97403 7.39166 9.37878 5.55997 12.2121 5.55997Z" fill="#EB4335"/>
                     </svg>
                     <p>Log in with Google</p>
-                </div>
+                </div> */}
+                <GoogleButton />
                 <div className=' w-full mt-10 flex gap-2 text-[#475569] text-sm font-medium justify-center items-center ' >
                     <p className=' ' >Log in with Google </p>
                     <a href='/signup' className=' font-semibold underline ' >Sign up</a> 
