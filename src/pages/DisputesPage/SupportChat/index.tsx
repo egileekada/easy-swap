e910a75f3f9fc819e901c9570618938430d202b3
egileekada <egileoniso.ekada@gmail.com>
2023-05-28 21:09:36 +0100

    update

--- src/pages/DisputesPage/SupportChat/index.tsx
+++ src/pages/DisputesPage/SupportChat/index.tsx
@@ -5,6 +5,8 @@ import { useNavigate } from 'react-router-dom'
 
 export default function SupportChat() {
 
+
+    const [subject, setSubject] = React.useState("")
     const [message, setMessage] = React.useState("")
     const [loading, setLoading] = React.useState(false)
     const navigate = useNavigate()
@@ -45,6 +47,15 @@ export default function SupportChat() {
     return (
         <div className=' w-full flex flex-col items-center pb-6 pt-4 ' >
             <div className=' w-full lg:px-0 px-6 lg:w-[600px] ' >
+                <div className=' w-full mb-5 ' > 
+                    <p className=' mb-2 text-[#475467] font-normal   ' >Ticket Type</p>
+                    <Select onChange={(e)=> setSubject(e.target.value)} placeholder='Select Ticket Type' fontSize="sm" backgroundColor="white"  >
+                        <option>Payment confirmation</option>
+                        <option>Bank details</option>
+                        <option>OTP Issues</option>
+                        <option>Others</option>
+                    </Select>
+                </div>
                 <textarea onChange={(e)=> setMessage(e.target.value)} className=' p-4 border w-full h-[200px] ' />
                 <div className=' w-full flex pt-4 justify-end ' >
                     <button onClick={submit} disabled={(message || !loading) ? false:true} className={` h-[50px] text-sm font-semibold w-[220px] ${message ? "bg-[#303179] text-white": "bg-[#F2F4F7] text-[#475467] "} rounded-lg `} >
