5b4b0e82943895e7d3ae40b0cef9423cf221ac71
egileekada <egileoniso.ekada@gmail.com>
2023-06-14 14:31:41 +0100

    updates

--- src/components/HomePageComponents/HeroSection/index.tsx
+++ src/components/HomePageComponents/HeroSection/index.tsx
@@ -43,7 +43,7 @@ export default function HeroSection() {
                     <motion.div  ref={ref} initial={{x: -200}} animate={controls}  
                         transition={{ ease: "easeOut", duration: 1 }}  className=' w-full pt-8 ' >
                         <h1 className=' text-[#303179] text-3xl lg:text-5xl xl:text-6xl leading-[1.1] font-bold ' >Buy, sell, and swap your cryptocurrencies with ease.</h1>
-                        <p className=' font-normal mt-4 ' >Eyswap enables you to sell your crypto and receive Naira instantly. No waits or delays!! Sign-up today and experience the power of quick payments.</p>
+                        <p className=' font-normal mt-4 ' >Ezyswap enables you to sell your crypto and receive Naira instantly. No waits or delays!! Sign-up today and experience the power of quick payments.</p>
                         <button onClick={()=> navigate("/dashboard/sellcrypto")} className=' mt-8 border-[#303179] border font-semibold text-[#303179] rounded-[10px] px-5 h-[46px] ' >Swap Now</button>
                     </motion.div>
                     <motion.div ref={ref2} initial={{ scale: 0}} animate={controls2}  
